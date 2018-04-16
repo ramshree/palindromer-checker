e119cbc20d73fa6a1482dbff816477ac0dfd060c
ramshree <shreedharus@gmail.com>
2018-04-15 23:15:02 -0400

    checkin

--- PalindromeChecker/settings.py
+++ PalindromeChecker/settings.py
@@ -113,12 +113,15 @@ USE_L10N = True
 
 USE_TZ = True
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
+    os.path.join(PROJECT_ROOT, "static")
 ]
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
