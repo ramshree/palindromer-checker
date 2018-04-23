a6a7ddd43069392ccb488e833813db43506f8921
ramshree <shreedharus@gmail.com>
2018-04-22 23:51:01 -0400

    Update to settings.py

--- PalindromeChecker/settings.py
+++ PalindromeChecker/settings.py
@@ -117,7 +117,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
